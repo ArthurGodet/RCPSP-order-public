d2ae8c29354740dfb65eb0d6269efb03dc6e50cf
Arthur Godet <arth.godet@gmail.com>
2021-06-22 16:51:38 +0200

    remove some useless code for this stand-alone project

--- src/main/java/data/InstanceSP.java
+++ src/main/java/data/InstanceSP.java
@@ -4,11 +4,7 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import gnu.trove.list.array.TIntArrayList;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Random;
 
 public class InstanceSP {
     private final String name;
@@ -21,10 +17,6 @@ public class InstanceSP {
     private int[][] predecessors = null;
     @JsonIgnore
     private int[][] successors = null;
-    @JsonIgnore
-    private int[] successorsChainLength = null;
-    @JsonIgnore
-    private int[] successorsChainLengthWithTime = null;
 
     @JsonCreator
     public InstanceSP(
@@ -118,125 +110,8 @@ public class InstanceSP {
         return this.successors;
     }
 
-    private int getSuccessorsLength(int i, boolean withTime) {
-        int toAdd = (withTime ? activities.get(i).getDuration() : 1);
-        if(activities.get(i).getSuccessors().length == 0) {
-            return toAdd;
-        } else {
-            return toAdd + Arrays.stream(activities.get(i).getSuccessors()).map(k -> getSuccessorsLength(k, withTime)).max().getAsInt();
-        }
-    }
-
-    private void buildSuccessorsChainLength() {
-        successorsChainLength = new int[activities.size()];
-        for(int i = 0; i < successorsChainLength.length; i++) {
-            successorsChainLength[i] = getSuccessorsLength(i, false);
-        }
-    }
-
-    private void buildSuccessorsChainLengthWithTime() {
-        successorsChainLengthWithTime = new int[activities.size()];
-        for(int i = 0; i < successorsChainLengthWithTime.length; i++) {
-            successorsChainLengthWithTime[i] = getSuccessorsLength(i, true);
-        }
-    }
-
-    public int[] getSuccessorsChainLengthWithTime() {
-        if(this.successorsChainLengthWithTime == null) {
-            buildSuccessorsChainLengthWithTime();
-        }
-        return this.successorsChainLengthWithTime;
-    }
-
-    public int[] getSuccessorsChainLength() {
-        if(this.successorsChainLength == null) {
-            buildSuccessorsChainLength();
-        }
-        return this.successorsChainLength;
-    }
-
     public int getSize() {
         return size;
     }
 
-    ////////////////////////////////////////////////////////////////
-    ////////////////////// Instance generator //////////////////////
-    ////////////////////////////////////////////////////////////////
-
-    private static final Random RND = new Random(0);
-    private static final TIntArrayList list = new TIntArrayList();
-    private static final TIntArrayList list2 = new TIntArrayList();
-
-    private static int generateNonZeroInteger(int max) {
-        int a;
-        do {
-            a = RND.nextInt(max);
-        } while(a == 0);
-        return a;
-    }
-
-    private static InstanceSP generateInstance(int size, int maxDuration, int density, String name) {
-        list.clear();
-        for(int i = 0; i < size; i++) {
-            for(int j = i + 1; j < size; j++) {
-                list.add(i*size + j);
-            }
-        }
-        list.shuffle(RND);
-        List<Activity> activities = new ArrayList<>();
-        int horizon = 0;
-        int[] resourceAvailabilities = new int[4];
-        for(int j = 0; j < resourceAvailabilities.length; j++) {
-            resourceAvailabilities[j] = generateNonZeroInteger(10);
-        }
-        for(int i = 0; i < size; i++) {
-            int duration = generateNonZeroInteger(maxDuration);
-            horizon += duration;
-            int[] resourceConsumption = new int[resourceAvailabilities.length];
-            do {
-                for(int j = 0; j < resourceAvailabilities.length; j++) {
-                    if(resourceAvailabilities[j] > 1) {
-                        resourceConsumption[j] = RND.nextInt(resourceAvailabilities[j]);
-                    } else {
-                        resourceConsumption[j] = 1;
-                    }
-                }
-            } while(Arrays.stream(resourceConsumption).sum() == 0);
-            list2.clear();
-            for(int k = 0; k < list.size() * density / 100; k++) {
-                int tmp = list.getQuick(k);
-                if(tmp / size == i) {
-                    list2.add(tmp % size);
-                }
-            }
-            list2.sort();
-            Activity activity = new Activity(i, duration, resourceConsumption, list2.toArray());
-            activities.add(activity);
-        }
-        return new InstanceSP(name, "RCPSP", activities, resourceAvailabilities, horizon);
-    }
-
-    public static void main(String[] args) throws IOException {
-        /*
-        FileWriter fw = new FileWriter("visu.cmd");
-        for(int density = 10; density <= 100; density += 10) {
-            InstanceSP instanceSP = generateInstance(20, 30, density, "test_"+density);
-            String str = "data/RCPSP/test/"+instanceSP.getName();
-            Factory.toFile(str+".json", instanceSP);
-            Factory.writeWorldGVFile(new File(str+".json"));
-            fw.write("dot -x -Tpng -o"+str+".png "+str+".gv\n");
-        }
-        fw.close();
-        //*/
-        for(int size = 10; size <= 50; size += 5) {
-            for(int density = 10; density <= 80; density += 10) {
-                for(int k = 0; k < 5; k++) {
-                    System.out.println("rcpsp_"+size+"_"+density+"_"+k);
-                    InstanceSP instanceSP = generateInstance(size, 30, density, "rcpsp_"+size+"_"+density+"_"+k);
-                    String str = "data/RCPSP/rnd/"+instanceSP.getName();
-                    Factory.toFile(str+".json", instanceSP);
-                }
-            }
-        }
-    }
 }
